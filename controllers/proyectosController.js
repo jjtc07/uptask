a55ebc77382d02a7a8afafe87402a429491c07b8
Jesús Torres <jesustc17@gmail.com>
2019-10-27 08:51:56 -0400

    instalando y confirando pug como template de vistas para la aplicación

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,5 +1,5 @@
 exports.proyectosHome = (req, res) => {
-  res.send('Inicio controller');
+  res.render('index');
 }
 
 exports.nosotros = (req, res) => {
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path')
 
 // crear una app de express
 const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
+
+// habilitar pug
+app.set('view engine', 'pug');
+
+// Añadir la carpeta de las vistas
+// app.set('views', path.join(__dirname, './views')); // es opcional o para settear la carpeta por default ya que sin esta linea funciona correctamente.
 
 // ruta para el home
 app.use('/', routes());
